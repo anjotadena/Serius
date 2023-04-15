4a38a9eabfd1db4f4f1659b61d1905f1e8426aa5
Anjo Tadena <tadena.anjo@gmail.com>
2023-04-15 20:41:54 +0800

    Add product entity and controllerss

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "Get all products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return $"Get product by ID: {id}";
+        }
+    }
+}
