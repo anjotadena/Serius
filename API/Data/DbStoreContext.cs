63a35222365df5d9fb7650bc6b4597224ff31087
Anjo Tadena <tadena.anjo@gmail.com>
2023-04-15 20:53:19 +0800

    Create db context class

---
+++ API/Data/DbStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DbStoreContext : DbContext
+    {
+        public DbStoreContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
