1065670c6cfc62b293298e21059f409a7c3493cf
Anjo Tadena <tadena.anjo@gmail.com>
2023-04-16 08:57:03 +0800

    Apply migration during startup

--- API/Program.cs
+++ API/Program.cs
@@ -32,4 +32,20 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+var context = services.GetRequiredService<DbStoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+
+try
+{
+    await context.Database.MigrateAsync();
+}
+catch (Exception ex)
+{
+    logger.LogError(ex, "An error during migrations");
+}
+
 app.Run();
