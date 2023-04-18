6c3941438705cb707b38b09d6df5aeff4393de90
Anjo Tadena <tadena.anjo@gmail.com>
2023-04-19 01:19:12 +0800

    Add product return DTO

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Core.Interfaces;
 using Infrastructure.Repositories;
 using Core.Specification;
+using API.Dtos;
 
 namespace API.Controllers
 {
@@ -19,19 +20,45 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<List<ProductReturnDto>>> GetProducts()
         {
             var spec = new ProductsWithTypesAndBrandSpecification();
+            var products = await _productRepository.ListAsync(spec);
 
-            return Ok(await _productRepository.ListAsync(spec));
+            return Ok(products.Select(product => new ProductReturnDto
+            {
+                Id = product.Id,
+                Title = product.Title,
+                Description = product.Description,
+                Price = product.Price,
+                Type = product.Type.Name,
+                Brand = product.Brand.Name,
+                Rating = product.Rating,
+                DiscountPercentage = product.DiscountPercentage,
+                Thumbnail = product.Thumbnail,
+                Stock = product.Stock
+            }).ToList());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
+        public async Task<ActionResult<ProductReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandSpecification(id);
+            var product = await _productRepository.GetEntityWithSpec(spec);
 
-            return Ok(await _productRepository.GetEntityWithSpec(spec));
+            return Ok(new ProductReturnDto
+            {
+                Id = product.Id,
+                Title = product.Title,
+                Description = product.Description,
+                Price = product.Price,
+                Type = product.Type.Name,
+                Brand = product.Brand.Name,
+                Rating = product.Rating,
+                DiscountPercentage = product.DiscountPercentage,
+                Thumbnail = product.Thumbnail,
+                Stock = product.Stock
+            });
         }
     }
 }
---
+++ API/Dtos/ProductReturnDto.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+
+namespace API.Dtos
+{
+    public class ProductReturnDto
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal DiscountPercentage { get; set; }
+
+        public decimal Rating { get; set; }
+
+        public string? Thumbnail { get; set; }
+
+        public ICollection<string>? Images { get; set; }
+
+        public int Stock { get; set; }
+
+        public string Type { get; set; }
+
+        public string Brand { get; set; }
+    }
+}
